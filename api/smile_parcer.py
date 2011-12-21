23a5f520684fa8fca6627931b46dc0fb2e4c498c
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-12-21 21:30:07 +0400

    Bug fix

--- api/smile_parcer.py
+++ api/smile_parcer.py
@@ -7,8 +7,9 @@ dir = '/Users/vladimir/PycharmProjects/my-chat2/static/smiles'
 for root, dirs, files in os.walk(dir):
     my_string = str()
     for name in files:
-        #print '\'' + ':' + name[:-4] + ':' + '\'' + " : " + '\'<img src=' + "\\" + '\'/static/smiles/standart/' + name + '\\' + '\'' + '/>' + '\'' + ','
+#        print '\'' + ':' + name[:-4] + ':' + '\'' + " : " + '\'<img src=' + "\\" + '\'/static/smiles/standart/' + name + '\\' + '\'' + '/>' + '\'' + ','
         print """<a href="#" title=" """ + '\"' + name[:-4] +'\">'
-        print """            <img src="{{ static_url("smiles/standart/""" + name + """") }}" alt=":""" + name[:-4] + """:"/>"""
-        print "       </a> "
+        print """<img src="{{ static_url("smiles/standart/""" + name + """") }}" alt=":""" + name[:-4] + """:"/>"""
+        print "</a> "
         my_string +=':' + name[:-4] +':' + "|"
+    print my_string
