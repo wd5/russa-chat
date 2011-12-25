140f3abb4ba8ee445217ac8c95a6cca66af8144d
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-12-25 14:27:58 +0400

    Bug fix

--- chat.py
+++ chat.py
@@ -143,16 +143,18 @@ class AuthLoginHandler(BaseHandler):
               return
           p = re.compile(u'^[a-zA-Z0-9]*$|^[а-яА-Я0-9]*$')
           m = p.match(name)
-          print "aaaaa"
           if m:
               print "bbbbb"
               for waiter in ChatConnection.waiters:
                   if str(waiter.user_name) == name.encode('utf-8'):
+                      print "11111"
                       self.render("login.html", error="Такое имя уже используется")
                       return
               if User.objects.filter(username=name):
+                  print "99999"
                   self.render("login.html", error="Такое имя уже используется")
                   return
+              print "oooooo"
               self.set_secure_cookie("user", name)
               self.set_secure_cookie("user_id", str(uuid.uuid4()))
               self.redirect("/")
