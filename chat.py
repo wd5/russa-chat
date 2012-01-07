bd7fd647b97457fcb720abcf3c75b62311bfd7e5
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2012-01-07 16:53:07 +0400

    Bug fix

--- chat.py
+++ chat.py
@@ -521,8 +521,7 @@ class VKHandler(BaseHandler, VKMixin):
   def _on_test(self, response):
       print response
       print response['response']
-      print response['response']['sex']
-      print response['response']['0']
+      print response['response'][0]
       self.redirect("/")
 
 class VKTest(BaseHandler, VKMixin):
