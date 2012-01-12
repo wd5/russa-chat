15eeb5cdf741305ca3e647ae8b0019566c3a9e80
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2012-01-12 22:20:30 +0400

    Bug fix

--- chat.py
+++ chat.py
@@ -371,7 +371,7 @@ class ChatConnection(tornadio2.conn.SocketConnection):
                     for waiter in self.waiters:
                         if waiter.user_id == input['value']:
                             private_to = waiter.user_name
-                    print u'Приват от %s для %s: %s' % (self.user_name, private_to, message["message"])
+                    print u'Приват от %s для : ' % self.user_name
                     message1 = {
                         "private" : "True",
                         "type": "new_message",
